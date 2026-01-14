b21cd078de5774b845d0df40a717a2be76d7d630
yuanjun <yuanjun@zmexing.com>
2026-01-14 19:22:47 +0800

    适配最新sqlalchemy2.0

--- orm.py
+++ orm.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Table, MetaData, Column, Integer, String, Date, ForeignKey
-from sqlalchemy.orm import mapper, relationship
+from sqlalchemy.orm import registry, relationship
 
 import model
 
@@ -33,15 +33,23 @@ allocations = Table(
     Column("batch_id", ForeignKey("batches.id")),
 )
 
+# 创建全局注册表示例(1个项目创建1个即可)
+mapper_registry = registry()
+
 
 def start_mappers():
-    lines_mapper = mapper(model.OrderLine, order_lines)
-    mapper(
+    # 绑定 纯Python类 和 Table对象，实现经典映射
+    # 1. 映射OrderLines，替代原 mapper(model.OrderLine, order_lines)
+    lines_mapper = mapper_registry.map_imperatively(model.OrderLine, order_lines)
+    # 2. 映射 Batch，替代原 mapper(...)，里面的 properties 内部逻辑 完全不变！！！
+    mapper_registry.map_imperatively(
         model.Batch,
         batches,
         properties={
             "_allocations": relationship(
-                lines_mapper, secondary=allocations, collection_class=set,
+                lines_mapper, 
+                secondary=allocations, 
+                collection_class=set,  # 这个集合类型保留
             )
         },
     )
